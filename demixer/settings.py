36d896edc859edb93ad392b60c8f64b459dc5511
amadionix <amadionicola@gmail.com>
2020-04-15 16:57:40 +0200

    debug set to False and secretkey hidden

--- demixer/settings.py
+++ demixer/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=en56_l=6g+tr%9&cmrj-x+tqakqhn_*-u3kg$b-e!a8pyfxbp'
+# SECRET_KEY = '=en56_l=6g+tr%9&cmrj-x+tqakqhn_*-u3kg$b-e!a8pyfxbp'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '=en56_l=6g+tr%9&cmrj-x+tqakqhn_*-u3kg$b-e!a8pyfxbp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
