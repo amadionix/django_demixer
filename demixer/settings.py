b794ada6808225733ecc2c22963fa3236b4e87b8
amadionix <amadionicola@gmail.com>
2020-04-17 11:08:01 +0200

    removed outdated buildpack, added sys.path.append(path to ffmpeg/ffprobe) to settings

--- demixer/settings.py
+++ demixer/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import sys
+sys.path.append('~/.heroku/python/lib/python3.7/site-packages/ffmpeg')
+sys.path.append('~/.heroku/python/lib/python3.7/site-packages/ffprobe')
 
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
