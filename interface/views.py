76ab6edb936c6df3cb699b2e3ec1a662d03dca05
amadionix <amadionicola@gmail.com>
2020-04-19 18:03:46 +0200

    adding session name

--- interface/views.py
+++ interface/views.py
@@ -21,17 +21,15 @@ uploads_path = os.path.join(BASE_DIR, 'uploads')
 output_path = os.path.join(uploads_path, 'output')
 tmp = os.path.join(uploads_path, 'demixed_track')
 
-name = ""
-
 def home(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            global name
             new_song = Song(song=request.FILES['select_audio_file'])
             new_song.save()
             unparsed_name = new_song.song.name
             name = new_song.song.name = ''.join(c for c in unparsed_name if ord(c) < 128)
+            request.session['track_name'] = name
 
             return render(request, 'select.html')
     else:
@@ -50,12 +48,13 @@ def process2(request):
     return render(request, 'success.html')
 
 def process4(request):
+    name = request.session['track_name'] 
     separator = Separator('spleeter:4stems')
     separator.separate_to_file(os.path.join(uploads_path, name), output_path)
     return render(request, 'success.html')
 
 def download(request):
-    global name
+    name = request.session['track_name'] 
     stripped_name = os.path.splitext(name)[0]
     tmp = os.path.join(uploads_path, stripped_name)
     # shutil.make_archive(tmp, 'zip', os.path.join(output_path, stripped_name))
